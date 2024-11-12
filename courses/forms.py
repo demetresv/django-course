3612a43baa19455cf12424451f719725ffa34869
demes <ninucakott@gmail.com>
2024-11-12 22:26:29 +0400

    hello

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Course
+from .models import Course, Student
+
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['name', 'description']
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model=Student
+        fields=['name','lastname','age','course']
--- courses/models.py
+++ courses/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    age=models.IntegerField()
+    course=models.ForeignKey(Course, on_delete=models.CASCADE, related_name='student')
